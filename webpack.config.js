7342246acd7f823ed52a007c2e74f3a18440ec4f
Jozef Podlecki <jozef.witold.podlecki@gmail.com>
2020-05-10 22:51:18 +0100

    update base href

--- webpack.config.js
+++ webpack.config.js
@@ -2,60 +2,69 @@ const webpack = require('webpack');
 const path = require('path');
 const Dotenv = require('dotenv-webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');  
-  
-module.exports = {  
-  mode: 'development',  
-  entry: {  
-    app: './src/index.ts'  
-  },
-  output: {
-    publicPath: '/',
-    path: __dirname,
-    filename: "[name].bundle.js",
-  },
-  devtool: 'source-map',
-  devServer: {  
-    contentBase: './',  
-    overlay: true
-  },  
-  resolve: {  
-    extensions: ['.js', '.ts', '.styl', 'pug']  
-  }, 
-  module: {  
-    rules: [  
-      {  
-        test: /\.ts$/,  
-        exclude: /node_modules/,  
-        loader: 'ts-loader'  
-      },
-      {
-        test: /\.(png|svg|jpg|gif|jfif)$/,
-        use: [
-          'file-loader',
-        ],
-      },
-      { 
-        test: /\.pug$/,
-        use: ["pug-loader"]
-      },
-      {
-        test: /\.styl$/,
-        loader: [
-            'style-loader',
-            'css-loader',
-            'stylus-loader'
-        ]
-      },
-      {  
-        test: /\.html$/,
-        use: 'html-loader'
-      }  
-    ]  
-  },  
-  plugins: [
-    new Dotenv(),
-    new HtmlWebpackPlugin({  
-      template: './src/index.pug'  
-    }),  
-  ]
-};
+
+
+module.exports = (env, argv) => {
+
+  const dotenv = require('dotenv').config({path: `.env.${argv.mode}`}).parsed
+
+  return {
+    mode: 'development',  
+    entry: {  
+      app: './src/index.ts'  
+    },
+    output: {
+      publicPath: '/',
+      path: __dirname,
+      filename: "[name].bundle.js",
+    },
+    devtool: 'source-map',
+    devServer: {  
+      contentBase: './',  
+      overlay: true
+    },  
+    resolve: {  
+      extensions: ['.js', '.ts', '.styl', 'pug']  
+    }, 
+    module: {  
+      rules: [  
+        {  
+          test: /\.ts$/,  
+          exclude: /node_modules/,  
+          loader: 'ts-loader'  
+        },
+        {
+          test: /\.(png|svg|jpg|gif|jfif)$/,
+          use: [
+            'file-loader',
+          ],
+        },
+        { 
+          test: /\.pug$/,
+          loader: "pug-loader"
+        },
+        {
+          test: /\.styl$/,
+          loader: [
+              'style-loader',
+              'css-loader',
+              'stylus-loader'
+          ]
+        },
+        {  
+          test: /\.html$/,
+          use: 'html-loader'
+        }  
+      ]  
+    },  
+    plugins: [
+      new Dotenv(),
+      new HtmlWebpackPlugin({  
+        template: '!!pug-loader!src/index.pug',
+        baseUrl: dotenv.BaseHref,
+        appName: dotenv.AppName,
+        googleMaps: dotenv.GoogleMaps,
+      }),  
+    ]
+  }
+}
